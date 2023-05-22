d0f3d639a9c34bd3f9e5271f8db9956629b78a63
squirre17 <3319277663@qq.com>
2023-05-22 10:29:13 +0800

    modify akd to autokd

--- autokd/krunner.py
+++ autokd/krunner.py
@@ -8,11 +8,11 @@ import shutil
 import autokd.utils.printer as printer
 import subprocess as sp
 
-from tqdm             import (tqdm)
-from typing           import (Type)
-from pathlib          import (Path)
-from typing           import (List)
-from loguru           import (logger)
+from tqdm                import (tqdm)
+from typing              import (Type)
+from pathlib             import (Path)
+from typing              import (List)
+from loguru              import (logger)
 from autokd.utils.dynbar import (Dynbar)
 from autokd.config       import (config)
 
